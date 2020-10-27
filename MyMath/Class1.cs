baf273d93662eb35a1c44eadc5161ae4ef7c4a09
Kiran Anand <liteninkiran@hotmail.com>
2020-10-27 20:27:20 +0000

    Add unit test & code stub

--- MyMath/Class1.cs
+++ MyMath/Class1.cs
@@ -5,4 +5,12 @@ namespace MyMath
     public class Class1
     {
     }
+
+    public class Rooter
+    {
+        public double SquareRoot(double input)
+        {
+            return input / 2;
+        }
+    }
 }
