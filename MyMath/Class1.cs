a662da663ba852649b70a3b09f344cb392206833
Kiran Anand <liteninkiran@hotmail.com>
2020-10-27 20:32:34 +0000

    Test for negative input

--- MyMath/Class1.cs
+++ MyMath/Class1.cs
@@ -10,6 +10,11 @@ namespace MyMath
     {
         public double SquareRoot(double input)
         {
+            if (input <= 0.0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
             double result = input;
             double previousResult = -input;
             while (Math.Abs(previousResult - result) > result / 1000)
